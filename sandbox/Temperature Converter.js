9c3ddb3f8a84fa760ed308e81e111fa5713dd968
Megan Brown <mdavi@megandavi.tech>
2018-08-03 18:13:28 -0500

    Initial commit

---
+++ sandbox/Temperature Converter.js
@@ -0,0 +1,10 @@
+let fahrenheit = 32
+
+// Celsius conversion
+let celsius = (fahrenheit - 32) * 1.8
+
+// Kelvin conversion
+let kelvin =  (fahrenheit + 459.67) * (5/9)
+
+console.log("The temperature " + fahrenheit + "°F converts to "
+            + celsius + "°C and " + kelvin + " K.")
